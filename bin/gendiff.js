6844074a612fab7cf186f6750d6588e330fb61b4
kat-in <newkatelife@gmail.com>
2025-02-11 21:08:11 +0300

    Добавлены .gitignore и makefile, установлен commander js и создан исполняемый gendiff.js, который при запуске с флагом -h выводит справку

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import { program } from "commander";
+
+program
+  .description("Compares two configuration files and shows a difference.")
+  .version("0.0.1", "-V, --version", "output the version number")
+  .helpOption("-h, --help", "output usage information");
+
+program.parse();
