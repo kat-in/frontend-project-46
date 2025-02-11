22c951435597b8033374047e50bf050a23d7b43c
kat-in <newkatelife@gmail.com>
2025-02-11 21:30:22 +0300

    Добавлены аргументы- пути к файлам и формат

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,8 +2,11 @@
 import { program } from "commander";
 
 program
+  .argument("<filepath1>")
+  .argument("<filepath2>")
   .description("Compares two configuration files and shows a difference.")
   .version("0.0.1", "-V, --version", "output the version number")
+  .option("-f, --format [type]", "output format")
   .helpOption("-h, --help", "output usage information");
 
 program.parse();
