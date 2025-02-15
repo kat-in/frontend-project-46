02f37b294d678db79d4d59972f13f7862f1da955
kat-in <newkatelife@gmail.com>
2025-02-15 21:46:53 +0300

    feat: Добавлен eslint и jest, папки __fixtures__ и __tests__. Добавлены файлы json и yml, реализован их парсинг

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 import { program } from "commander";
+import { readFileSync } from "node:fs";
+import { cwd } from "node:process";
+import path from "node:path";
+import genDiff from "./index.js";
+import yaml from "js-yaml";
 
 program
   .argument("<filepath1>")
@@ -7,6 +12,20 @@ program
   .description("Compares two configuration files and shows a difference.")
   .version("0.0.1", "-V, --version", "output the version number")
   .option("-f, --format [type]", "output format")
+  .action((filepath1, filepath2) => {
+    const format = (filepath) => {
+      const newPath = path.resolve(cwd(), filepath);
+      let file;
+      if (filepath.endsWith(".json")) {
+        file = JSON.parse(readFileSync(newPath, { encoding: "UTF-8" }));
+      }
+      if (filepath.endsWith(".yml")) {
+        file = yaml.load(readFileSync(newPath, { encoding: "UTF-8" }));
+      }
+      return file;
+    };
+    console.log(genDiff(format(filepath1), format(filepath2)));
+  })
   .helpOption("-h, --help", "output usage information");
 
 program.parse();
---
+++ src/genDiff.js
@@ -0,0 +1,5 @@
+const genDiff = (file1, file2) => {
+  return [file1, file2];
+};
+
+export default genDiff;
