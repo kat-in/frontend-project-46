a587e45d5920c4a803a099571e8e4054e837d70d
kat-in <newkatelife@gmail.com>
2025-02-16 15:54:22 +0300

    fix linter bugs

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
-import { program } from "commander";
-import genDiff from "../index.js";
-import getParserType from "../src/getParserType.js";
+import { program } from 'commander';
+import genDiff from '../index.js';
+import getParserType from '../src/getParserType.js';
 
 program
-  .argument("<filepath1>")
-  .argument("<filepath2>")
-  .description("Compares two configuration files and shows a difference.")
-  .version("0.0.1", "-V, --version", "output the version number")
-  .option("-f, --format [type]", "output format")
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
     const options = program.opts();
     console.log(
       genDiff(
         getParserType(filepath1),
         getParserType(filepath2),
-        options.format
-      )
+        options.format,
+      ),
     );
   })
-  .helpOption("-h, --help", "output usage information");
+  .helpOption('-h, --help', 'output usage information');
 
 program.parse();
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,11 +1,11 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-const genDiff = (file1, file2, type = "stylish") => {
+const genDiff = (file1, file2, type = 'stylish') => {
   const keys1 = _.sortBy(Object.keys(file1));
   const keys2 = _.sortBy(Object.keys(file2));
   const keys = _.union(keys1, keys2);
 
-  if (type === "stylish") {
+  if (type === 'stylish') {
     const difference = keys.flatMap((key) => {
       const result = [];
       if (!_.includes(keys2, key)) {
@@ -20,17 +20,17 @@ const genDiff = (file1, file2, type = "stylish") => {
       }
       return result;
     });
-    return `{\n${_.join(difference, "\n")}\n}`;
+    return `{\n${_.join(difference, '\n')}\n}`;
   }
 
-  if (type === "plain") {
+  if (type === 'plain') {
     const difference = keys.flatMap((key) => {
       const result = [];
       if (!_.includes(keys2, key)) {
         result.push(`Property '${[key]}' was removed`);
       } else if (!_.includes(keys1, key)) {
         result.push(
-          `Property '${[key]}' was added with value: '${file2[key]}'`
+          `Property '${[key]}' was added with value: '${file2[key]}'`,
         );
       } else if (file1[key] === file2[key]) {
         result.push(`Property '${[key]}' was not updated.`);
@@ -38,14 +38,14 @@ const genDiff = (file1, file2, type = "stylish") => {
         result.push(
           `Property '${[key]}' was updated. From: '${file1[key]}' to '${
             file2[key]
-          }'`
+          }'`,
         );
       }
       return result;
     });
-    return _.join(difference, "\n");
+    return _.join(difference, '\n');
   }
-  throw new Error("The selected type is not supported");
+  throw new Error('The selected type is not supported');
 };
 
 export default genDiff;
