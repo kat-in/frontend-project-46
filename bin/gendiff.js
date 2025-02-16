0e95344a6232ccb5bf2cb60930b0e99ce328f3b5
kat-in <newkatelife@gmail.com>
2025-02-16 12:45:38 +0300

    добавлена реализация сравнения плоских файлов в двух форматах: stylish и plain, записана аскинема

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,26 +3,25 @@ import { program } from "commander";
 import { readFileSync } from "node:fs";
 import { cwd } from "node:process";
 import path from "node:path";
-import genDiff from "./index.js";
 import yaml from "js-yaml";
+import genDiff from "../index.js";
 
 program
   .argument("<filepath1>")
   .argument("<filepath2>")
+  .argument("[type]")
   .description("Compares two configuration files and shows a difference.")
   .version("0.0.1", "-V, --version", "output the version number")
   .option("-f, --format [type]", "output format")
   .action((filepath1, filepath2) => {
     const format = (filepath) => {
       const newPath = path.resolve(cwd(), filepath);
-      let file;
       if (filepath.endsWith(".json")) {
-        file = JSON.parse(readFileSync(newPath, { encoding: "UTF-8" }));
+        return JSON.parse(readFileSync(newPath, { encoding: "UTF-8" }));
       }
       if (filepath.endsWith(".yml")) {
-        file = yaml.load(readFileSync(newPath, { encoding: "UTF-8" }));
+        return yaml.load(readFileSync(newPath, { encoding: "UTF-8" }));
       }
-      return file;
     };
     console.log(genDiff(format(filepath1), format(filepath2)));
   })
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,5 +1,50 @@
-const genDiff = (file1, file2) => {
-  return [file1, file2];
+import _ from "lodash";
+
+const genDiff = (file1, file2, type = "stylish") => {
+  const keys1 = _.sortBy(Object.keys(file1));
+  const keys2 = _.sortBy(Object.keys(file2));
+  const keys = _.union(keys1, keys2);
+
+  if (type === "stylish") {
+    const difference = keys.flatMap((key) => {
+      const result = [];
+      if (!_.includes(keys2, key)) {
+        result.push(`  - ${[key]}: ${file1[key]}`);
+      } else if (!_.includes(keys1, key)) {
+        result.push(`  + ${[key]}: ${file2[key]}`);
+      } else if (file1[key] === file2[key]) {
+        result.push(`    ${[key]}: ${file1[key]}`);
+      } else {
+        result.push(`  - ${[key]}: ${file1[key]}`);
+        result.push(`  + ${[key]}: ${file2[key]}`);
+      }
+      return result;
+    });
+    return `{\n${_.join(difference, "\n")}\n}`;
+  }
+
+  if (type === "plain") {
+    const difference = keys.flatMap((key) => {
+      const result = [];
+      if (!_.includes(keys2, key)) {
+        result.push(`Property '${[key]}' was removed`);
+      } else if (!_.includes(keys1, key)) {
+        result.push(
+          `Property '${[key]}' was added with value: '${file2[key]}'`
+        );
+      } else if (file1[key] === file2[key]) {
+        result.push(`Property '${[key]}' was not updated.`);
+      } else {
+        result.push(
+          `Property '${[key]}' was updated. From: '${file1[key]}' to '${
+            file2[key]
+          }'`
+        );
+      }
+      return result;
+    });
+    return _.join(difference, "\n");
+  }
 };
 
 export default genDiff;
