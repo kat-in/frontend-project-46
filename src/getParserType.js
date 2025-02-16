d7d1c1b512d6d058112429bb1adc2e3b852b4b63
kat-in <newkatelife@gmail.com>
2025-02-16 15:26:54 +0300

    Функция определения парсера вынесена в отдельный модуль

---
+++ src/getParserType.js
@@ -0,0 +1,16 @@
+import { readFileSync } from "node:fs";
+import { cwd } from "node:process";
+import path from "node:path";
+import yaml from "js-yaml";
+
+const getParserType = (filepath) => {
+  const newPath = path.resolve(cwd(), filepath);
+  if (filepath.endsWith(".json")) {
+    return JSON.parse(readFileSync(newPath, { encoding: "UTF-8" }));
+  }
+  if (filepath.endsWith(".yml")) {
+    return yaml.load(readFileSync(newPath, { encoding: "UTF-8" }));
+  }
+};
+
+export default getParserType;
