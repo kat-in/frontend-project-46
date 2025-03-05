57575cf6ac058bdcb926e272473a0b2bb5e302a5
kat-in <newkatelife@gmail.com>
2025-03-05 15:08:35 +0300

    Исправлена точка входа

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-import path from 'node:path';
-import fs from 'fs';
-import parsers from './src/parsers.js';
-import buildTree from './src/buildTree.js';
-import getFormat from './src/formatters/index.js';
-
-const compareFiles = (file1, file2, format = 'stylish') => {
-  const content1 = fs.readFileSync(path.resolve(file1), 'UTF-8');
-  const content2 = fs.readFileSync(path.resolve(file2), 'UTF-8');
-  const firstObject = parsers(content1, path.extname(file1));
-  const secondObject = parsers(content2, path.extname(file2));
-  const compareObject = buildTree(firstObject, secondObject);
-  return getFormat(compareObject, format);
-
-  // return fs.writeFileSync("./__fixtures__/resultJson.json", data);
-};
+import compareFiles from './src/index.js';
 
 export default compareFiles;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import path from 'node:path';
+import fs from 'fs';
+import parsers from './parsers.js';
+import buildTree from './buildTree.js';
+import getFormat from './formatters/index.js';
+
+const compareFiles = (file1, file2, format = 'stylish') => {
+  const content1 = fs.readFileSync(path.resolve(file1), 'UTF-8');
+  const content2 = fs.readFileSync(path.resolve(file2), 'UTF-8');
+  const firstObject = parsers(content1, path.extname(file1));
+  const secondObject = parsers(content2, path.extname(file2));
+  const compareObject = buildTree(firstObject, secondObject);
+  return getFormat(compareObject, format);
+
+  // return fs.writeFileSync("./__fixtures__/resultJson.json", data);
+};
+
+export default compareFiles;
